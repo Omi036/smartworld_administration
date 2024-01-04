45d3c9261a4f959020d451891c20cbf486471869
Omi036 <omible@protonmail.com>
2024-01-04 22:10:42 +0100

    🔵 Added list-warns feature

---
+++ src/commands/settings/listwarns.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+    name:"advertencias",
+    description: "Añade una advertencia al usuario",
+    type: "command",
+    enabled: true,
+    data: new SlashCommandBuilder()
+        .setName("advertencias")
+        .setDescription("Muestra advertencias del usuario")
+        .addUserOption(option => 
+            option.setName("user")
+                .setDescription("Usuario al que buscar")
+                .setRequired(true)),
+
+    execute: async function(interaction, client, commands, dbservice) {
+
+        if(interaction.member.permissions.has("Administrator")) {
+            
+            const target = interaction.options.getUser("user")
+
+            const doc = await dbservice.colls.warnings.findOne({header: "warnings", target: target.id})
+
+            if(!doc) {
+                interaction.reply({content:`User <@${target.id}> no tiene ninguna advertencia`})
+                return
+            } 
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Advertencias de ${target.displayName}`)
+                .setDescription(`El usuario <@${target.id}> tiene las siguientes advertencias:`)
+                .setColor(0xffaa00)
+                .setThumbnail(target.avatarURL())
+
+
+            doc.value.forEach(warn => {
+                embed.addFields({
+                    name: `${warn.date}`,
+                    value:`**Emisor:** <@${warn.author}>\n**Receptor:** <@${warn.target}>\n**Sanción:** ${warn.gravedad} \n**Motivo:** ${warn.motivo}${warn.comentarios ? `\n**Comentarios:** ${warn.comentarios}` : ""}`
+                })
+            })
+
+            interaction.reply({embeds: [embed]})
+            
+
+        } else {
+            await interaction.reply("Debes tener permisos de `Administrador` para poder ejecutar este comando")
+        }
+    }
+}
